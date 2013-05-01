d0d4033cd429347c7a10585120c476768c43bff0
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-02 08:32:01 +0900

    Git.Interanl: raiseError with the description

--- Model/Git/Internal.hs
+++ Model/Git/Internal.hs
@@ -1,11 +1,18 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module Model.Git.Internal (Git) where
+module Model.Git.Internal
+       ( Git
+       , raiseError
+       )
+       where
 
+import Bindings.Libgit2.Errors
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Error
 import Control.Monad.Reader
+import Foreign
+import Foreign.C.String
 
 import Model.Git.Repository
 
@@ -18,3 +25,11 @@ newtype Git m a = Git (ErrorT String (ReaderT Repository m) a)
            , MonadError String
            , MonadReader Repository
            )
+
+raiseError :: MonadIO m => m a
+raiseError = liftIO $ do
+  msg  <- c'giterr_last
+  msg' <- maybePeek peek msg
+  fail =<< case msg' of
+    Nothing  -> return "unknow error"
+    Just err -> peekCString $ c'git_error'message err
