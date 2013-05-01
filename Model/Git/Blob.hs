f3a2bf4d79ab3d5aa3efbae77e8f952b7b5ce9a0
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-02 08:54:53 +0900

    Blob: Add blob

--- Model/Git/Blob.hs
+++ Model/Git/Blob.hs
@@ -1,1 +1,26 @@
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DataKinds #-}
+
 module Model.Git.Blob where
+
+import Bindings.Libgit2.Blob
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class
+import Foreign
+import Foreign.C.String
+
+import Model.Git.Internal
+import Model.Git.Oid
+
+
+blob :: MonadIO m => String -> Git m (Oid Blob)
+blob dat = do
+  repo <- repository
+  liftIO . alloca $ \blobOid           ->
+    withCString dat $ \dat'            ->
+    withForeignPtr repo $ \repository' -> do
+      let size = fromIntegral $ length dat
+      r <- c'git_blob_create_frombuffer blobOid repository' (castPtr dat') size
+      when (r < 0) raiseError
+      Oid <$> newForeignPtr_ blobOid
