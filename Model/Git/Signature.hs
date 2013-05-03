ae8a0ddf67f51d31b2f8fabcf471edf764d71ff4
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-03 21:56:34 +0900

    Signature: Add sign

--- Model/Git/Signature.hs
+++ Model/Git/Signature.hs
@@ -2,11 +2,20 @@ module Model.Git.Signature
        ( Signature
        , Email
        , Name
+       , sign
        )
        where
 
+import Bindings.Libgit2.Signature
 import Bindings.Libgit2.Types
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
 import Foreign
+import Foreign.C.String
+
+import Model.Git.Internal
+import Model.Git.Signature.Time
 
 
 type Signature = ForeignPtr C'git_signature
@@ -14,3 +23,20 @@ type Signature = ForeignPtr C'git_signature
 type Email = String
 
 type Name  = String
+
+sign :: MonadIO m => Name -> Email -> Time -> Git m Signature
+sign name email time = liftIO $ do
+  alloca $ \signature ->
+    withCString name $ \name'   ->
+    withCString email $ \email' -> do
+      r <- c'git_signature_new signature
+                               name'
+                               email'
+                               utcSeconds
+                               utcOffset
+      when (r < 0) raiseError
+      peek signature >>= newForeignPtr p'git_signature_free
+  where
+    utcSeconds = fromIntegral . round . utcTimeToPOSIXSeconds . utc $ time
+
+    utcOffset = fromIntegral . offset $ time
