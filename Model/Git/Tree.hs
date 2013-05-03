7a51a63bd681b60f4858f62873f98e33ac53b8f5
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-03 12:40:33 +0900

    Tree: Define Treeable type class

--- Model/Git/Tree.hs
+++ Model/Git/Tree.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DataKinds #-}
+
 module Model.Git.Tree where
 
 import Bindings.Libgit2.Tree
@@ -5,11 +8,30 @@ import Bindings.Libgit2.Types
 import Control.Monad
 import Control.Monad.IO.Class
 import Foreign
+import Foreign.C
+import Foreign.C.Types
 
 import Model.Git.Internal
+import Model.Git.Oid
 
 
 type TreeBuilder = ForeignPtr C'git_treebuilder
+
+class GitTreeable (a :: GitObject) where
+  filemode :: Oid a -> CUInt
+
+  insert :: (MonadIO m) => TreeBuilder -> String -> Oid a -> Git m ()
+  insert builder name oid@(Oid tree) = liftIO $
+    withForeignPtr builder $ \builder' ->
+    withCString name $ \filename'      ->
+    withForeignPtr tree $ \blobOid'    -> do
+      r <- c'git_treebuilder_insert nullPtr
+                                    builder'
+                                    filename'
+                                    blobOid'
+                                    (filemode oid)
+      when (r < 0) raiseError
+      return ()
 
 create :: MonadIO m => Git m TreeBuilder
 create = liftIO $ do
