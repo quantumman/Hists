7fccb364646fa03db1967baa1e990c9b4e944855
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-03 13:03:10 +0900

    Commit: Add withForeignPtrs

--- Model/Git/Commit.hs
+++ Model/Git/Commit.hs
@@ -1,1 +1,10 @@
 module Model.Git.Commit where
+
+import Foreign
+import Control.Monad
+
+
+withForeignPtrs :: [ForeignPtr a] -> ([Ptr a] -> IO b) -> IO b
+withForeignPtrs fptrs f = do
+  ptrs <- forM fptrs $ flip withForeignPtr return
+  f ptrs
