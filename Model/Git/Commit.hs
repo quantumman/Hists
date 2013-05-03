9a44b3e9fd27dae50518c4bd9842eed8de8a42c8
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-03 22:12:11 +0900

    Commit: Add commit'

--- Model/Git/Commit.hs
+++ Model/Git/Commit.hs
@@ -10,6 +10,7 @@ module Model.Git.Commit
        where
 
 import Bindings.Libgit2.Commit
+import Bindings.Libgit2.Tree
 import Bindings.Libgit2.Types
 import Control.Applicative
 import Control.Exception (bracket)
@@ -33,6 +34,60 @@ type Parents = [Oid Commit]
 type UpdateRef = String
 
 type Message = String
+
+commit' :: MonadIO m
+           => Oid Tree
+           -> Author
+           -> Committer
+           -> Parents
+           -> Maybe UpdateRef
+           -> Message
+           -> Git m (Oid Commit)
+commit' treeOid author committer parents updateRef message = do
+  repo <- repository
+  tree <- lookupTree repo treeOid
+  liftIO . alloca $ \ptr ->
+    withForeignPtr repo $ \repository'            ->
+    withForeignPtr tree $ \tree'                  ->
+    withForeignPtr author $ \author'              ->
+    withForeignPtr committer $ \committer'        ->
+    withParents repo parents $ \(size, parents')  ->
+    withUpdateRef updateRef $ \updateRef'         ->
+    withCString message $ \message'               -> do
+      r <- c'git_commit_create ptr
+                               repository'
+                               updateRef'
+                               author'
+                               committer'
+                               utf8
+                               message'
+                               tree'
+                               size
+                               parents'
+      when (r < 0) raiseError
+      Oid <$> newForeignPtr_ ptr
+  where
+    lookupTree repository (Oid treeOid) = liftIO $
+      withForeignPtr repository $ \repository' ->
+      withForeignPtr treeOid $ \treeOid'       ->
+      alloca $ \tree -> do
+        r <- c'git_tree_lookup tree repository' treeOid'
+        when (r < 0) raiseError
+        tree' <- peek tree
+        newForeignPtr_ tree'
+
+    withUpdateRef updateRef f = case updateRef of
+      Nothing  -> f nullPtr
+      Just ref -> withCString ref f
+
+    utf8 = nullPtr
+
+    withParents repo parents f =
+      withCommits repo parents $ \parents' -> do
+        let size = length parents'
+        liftIO $ allocaArray size $ \array -> do
+          pokeArray array parents'
+          f (fromIntegral size, array)
 
 withCommits :: Repository
                -> Parents
