8bf9348ac17246f0ee3ebfb1e73e213f5b95af91
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-03 13:09:38 +0900

    Signature: Define Signature

--- Model/Git/Signature.hs
+++ Model/Git/Signature.hs
@@ -1,1 +1,10 @@
-module Model.Git.Signature where
+module Model.Git.Signature
+       ( Signature
+       )
+       where
+
+import Bindings.Libgit2.Types
+import Foreign
+
+
+type Signature = ForeignPtr C'git_signature
