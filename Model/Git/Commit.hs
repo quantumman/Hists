8ed6b80b78f8ad917035b750d7847abac9e12756
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-03 22:03:50 +0900

    Commit: Define Parents

--- Model/Git/Commit.hs
+++ Model/Git/Commit.hs
@@ -1,6 +1,7 @@
 module Model.Git.Commit
        ( Author
        , Committer
+       , Parents
        )
        where
 
@@ -14,6 +15,8 @@ import Model.Git.Signature
 type Author = Signature
 
 type Committer = Signature
+
+type Parents = [Oid Commit]
 
 withForeignPtrs :: [ForeignPtr a] -> ([Ptr a] -> IO b) -> IO b
 withForeignPtrs fptrs f = do
