8dfaeb29d1dcf94cc0a43e45ce6c262d6612867b
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-05 22:53:11 +0900

    Reference: Add lookup

--- Model/Git/Reference.hs
+++ Model/Git/Reference.hs
@@ -1,11 +1,20 @@
 module Model.Git.Reference
        ( Reference(..)
        , ReferenceName
+       , Model.Git.Reference.lookup
        )
        where
 
+import Bindings.Libgit2.Oid
+import Bindings.Libgit2.Refs
+import Bindings.Libgit2.Types
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class
 import Foreign
+import Foreign.C.String
 
+import Model.Git.Internal
 import Model.Git.Oid
 
 type ReferenceName = String
@@ -17,3 +26,28 @@ data GitReferenceType = GitReferenceInvalid
                       | GitReferenceSymbolic
                       | GitReferenceListAll
   deriving (Show, Enum)
+
+lookup :: MonadIO m => ReferenceName -> Git m (Reference a)
+lookup referenceName = do
+  repo <- repository
+  lookup' repo >>= resolveType
+  where
+    lookup' repo = liftIO $
+      withForeignPtr repo $ \repository           ->
+      withCString referenceName $ \referenceName' ->
+      alloca $ \reference                         -> do
+        r <- c'git_reference_lookup reference repository referenceName'
+        when (r < 0) raiseError
+        peek reference >>= newForeignPtr p'git_reference_free
+
+    resolveType reference = liftIO $
+      withForeignPtr reference $ \reference' -> do
+        referenceType <- toEnum . fromIntegral <$> c'git_reference_type reference'
+        case referenceType of
+          GitReferenceOid      -> Direct . Oid <$>
+                                  (c'git_reference_target reference'
+                                   >>= newForeignPtr_)
+          GitReferenceSymbolic -> Symbolic <$>
+                                  (c'git_reference_symbolic_target reference'
+                                   >>= peekCString)
+          _                    -> raiseError
