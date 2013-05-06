881480a10296652ade213e38c220ab3f87d4a8e1
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-06 22:08:02 +0900

    Add Git.hs

---
+++ Model/Git.hs
@@ -0,0 +1,12 @@
+module Model.Git
+       ( module Git
+       )
+       where
+
+import Model.Git.Blob as Git
+import Model.Git.Commit as Git
+import Model.Git.Internal as Git
+import Model.Git.Oid as Git
+import Model.Git.Reference as Git
+import Model.Git.Repository as Git
+import Model.Git.Signature as Git
