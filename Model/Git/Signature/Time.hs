8476ac4aae21a62f776dd34e04adf58ed5c771bf
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-09 08:43:21 +0900

    Time: Add getCurrentLocalTime

--- Model/Git/Signature/Time.hs
+++ Model/Git/Signature/Time.hs
@@ -1,9 +1,22 @@
 module Model.Git.Signature.Time where
 
-import Data.Time (UTCTime)
+import Control.Applicative
+import Control.Monad.IO.Class
+import Data.Time (UTCTime, getCurrentTime)
+import Data.Time.LocalTime (TimeZone(..), getTimeZone)
+
+import Model.Git.Internal
 
 
 data Time = Time { utc :: UTCTime
                  , offset :: Int
                  }
   deriving (Show)
+
+getCurrentLocalTime :: MonadIO m => Git m Time
+getCurrentLocalTime = liftIO $ do
+  now    <- getCurrentTime
+  offset <- toSeconds . timeZoneMinutes <$> getTimeZone now
+  return $ Time now offset
+  where
+    toSeconds = (* 60)
