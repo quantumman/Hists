34ec2e599025ec139f75fc378ad587f44da4e5ae
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-12 22:59:12 +0900

    Fixture: Add fixtureSandbox

--- tests/Test/Hspec/Fixture.hs
+++ tests/Test/Hspec/Fixture.hs
@@ -1,8 +1,12 @@
 module Tests.Test.Hspec.Fixture where
 
 import Control.Monad ((=<<))
+import System.IO.Temp (withSystemTempDirectory)
 import Test.Hspec
 
 
 describe' :: String -> IO Spec -> IO Spec
 describe' msg = (return . describe msg =<<)
+
+fixtureSandbox :: String -> Spec -> IO Spec
+fixtureSandbox name = withSystemTempDirectory name . const . return
