c7f9660ea41ffd306c670f41d9c0f1b4eb421184
Shoadi Yokoyama <quantumcars@gmail.com>
2013-05-12 22:58:22 +0900

    Fixture: Add describe'

--- tests/Test/Hspec/Fixture.hs
+++ tests/Test/Hspec/Fixture.hs
@@ -1,1 +1,8 @@
-module Test.Hspec.Fixture where
+module Tests.Test.Hspec.Fixture where
+
+import Control.Monad ((=<<))
+import Test.Hspec
+
+
+describe' :: String -> IO Spec -> IO Spec
+describe' msg = (return . describe msg =<<)
